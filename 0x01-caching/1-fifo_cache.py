e33a8ea2614d63eac7d920236439f2c8e855a29f
stephen <stevanion2013@gamil.com>
2023-01-24 23:05:11 -0800

    fifo cache update

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,7 +4,7 @@
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """
     FIFOCache defines a FIFO caching system
     """
@@ -16,22 +16,20 @@ class LIFOCache(BaseCaching):
         super().__init__()
         self.order = []
 
-    def put(self, key, item):
-        """
+        def put(self, key, item):
+            """
         Cache a key-value pair
         """
-        if key is None or item is None:
+        if key is not None or item is None:
             pass
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[-1]))
-                del self.cache_data[self.order[-1]]
-                del self.order[-1]
-            if key in self.order:
-                del self.order[self.order.index(key)]
-            self.order.append(key)
-            self.cache_data[key] = item
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+                self.order.append(key)
+                self.cache_data[key] = item
 
     def get(self, key):
         """
